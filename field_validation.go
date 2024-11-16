5f78a65aacf8534803ddcec3ed278c067c27fcdd
Alex S. Garzão <alexgarzao@gmail.com>
2024-11-16 10:15:01 -0300

    feat: initial field validation code generation

---
+++ field_validation.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+var testFieldTpl = `	if %[1]s {
+		errs = append(errs, fmt.Errorf("%[2]s: %[3]s", ErrValidation))
+	}
+`
+
+type FieldValidation struct {
+	Name string
+	Type string
+	Tag  string
+}
+
+func (f *FieldValidation) Generate() (string, error) {
+	tag := f.Tag
+	tag, _ = strings.CutPrefix(tag, "validate:")
+	testCondition := ""
+	errorMessage := ""
+
+	// TODO: refactor
+	if tag == `"required"` {
+		if f.Type == "string" {
+			testCondition = "u." + f.Name + " == \"\""
+			errorMessage = f.Name + " required"
+		} else if f.Type == "uint8" {
+			testCondition = "u." + f.Name + " == 0"
+			errorMessage = f.Name + " required"
+		} else {
+			return "", fmt.Errorf("unsupported type %s", f.Type)
+		}
+	}
+
+	return fmt.Sprintf(testFieldTpl, testCondition, "%w", errorMessage), nil
+}
---
+++ field_validation_test.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/sergi/go-diff/diffmatchpatch"
+)
+
+func TestFieldValidationGenerate(t *testing.T) {
+	type fields struct {
+		Name string
+		Type string
+		Tag  string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		want    string
+		wantErr bool
+	}{
+		{
+			name: "String field required",
+			fields: fields{
+				Name: "FirstName",
+				Type: "string",
+				Tag:  "validate:\"required\"",
+			},
+			want: `	if u.FirstName == "" {
+		errs = append(errs, fmt.Errorf("%w: FirstName required", ErrValidation))
+	}
+`,
+			wantErr: false,
+		},
+		{
+			name: "Integer field required",
+			fields: fields{
+				Name: "MyNumber",
+				Type: "uint8",
+				Tag:  "validate:\"required\"",
+			},
+			want: `	if u.MyNumber == 0 {
+		errs = append(errs, fmt.Errorf("%w: MyNumber required", ErrValidation))
+	}
+`,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			f := &FieldValidation{
+				Name: tt.fields.Name,
+				Type: tt.fields.Type,
+				Tag:  tt.fields.Tag,
+			}
+			got, err := f.Generate()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("FieldValidation.Generate() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			if got != tt.want {
+				t.Errorf("FieldValidation.Generate() = %v, want %v", got, tt.want)
+				dmp := diffmatchpatch.New()
+				diffs := dmp.DiffMain(tt.want, got, false)
+				if len(diffs) > 1 {
+					t.Errorf("FieldValidation.Generate() diff = \n%v", dmp.DiffPrettyText(diffs))
+				}
+			}
+		})
+	}
+}
