61f5def8b122c59e21ddb952aa2843aff874406a
Alex S. Garzão <alexgarzao@gmail.com>
2024-11-20 11:51:18 -0300

    refac: using go templates
    With templates, a lot of code was removed or improved.
    At this time, StructInfo as all the information to generate the
    package definitions and struct validator func code.

--- code_generator.go
+++ code_generator.go
@@ -1,11 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"os"
-	"strings"
-)
-
 func generateCode(structs []StructInfo) error {
 	// TODO: validate tags ok?
 
@@ -14,54 +8,13 @@ func generateCode(structs []StructInfo) error {
 			continue
 		}
 
-		if err := generatePackageDefinition(structInfo); err != nil {
+		if err := structInfo.GenerateFilePackageDefinition(); err != nil {
 			return err
 		}
 
-		if err := generateFileValidator(structInfo); err != nil {
+		if err := structInfo.GenerateFileValidator(); err != nil {
 			return err
 		}
-	}
-
-	return nil
-}
-
-func generatePackageDefinition(structInfo StructInfo) error {
-	fmt.Println("Generating package definitions code")
-
-	pd := &PackageDefinitions{
-		PackageName: structInfo.PackageName,
-	}
-
-	code, err := pd.Generate()
-	if err != nil {
-		return err
-	}
-
-	if err := os.WriteFile(structInfo.Path+"/validators.go", []byte(code), 0644); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func generateFileValidator(structInfo StructInfo) error {
-	fmt.Printf("Generating struct %s validations code\n", structInfo.Name)
-
-	fv := &FileValidator{
-		FileHeader: FileHeader{
-			PackageName: structInfo.PackageName,
-		},
-		StructInfo: structInfo,
-	}
-
-	code, err := fv.Generate()
-	if err != nil {
-		return err
-	}
-
-	if err := os.WriteFile(structInfo.Path+"/"+strings.ToLower(structInfo.Name)+"_validator.go", []byte(code), 0644); err != nil {
-		return err
 	}
 
 	return nil
--- struct_info_test.go
+++ struct_info_test.go
@@ -6,11 +6,9 @@ import (
 	"github.com/sergi/go-diff/diffmatchpatch"
 )
 
-func TestFuncValidatorGenerate(t *testing.T) {
+func TestStructInfoGenerateValidator(t *testing.T) {
 	type fields struct {
-		Name              string
-		FieldsValidations []FieldInfo
-		HasValidateTag    bool
+		StructInfo StructInfo
 	}
 	tests := []struct {
 		name    string
@@ -21,29 +19,38 @@ func TestFuncValidatorGenerate(t *testing.T) {
 		{
 			name: "Valid struct",
 			fields: fields{
-				Name: "User",
-				FieldsValidations: []FieldInfo{
-					{
-						Name: "FirstName",
-						Type: "string",
-						Tag:  "validate:\"required\"",
+				StructInfo: StructInfo{
+					Name: "User",
+					FieldsInfo: []FieldInfo{
+						{
+							Name: "FirstName",
+							Type: "string",
+							Tag:  `validate:"required"`,
+						},
+						{
+							Name: "MyAge",
+							Type: "uint8",
+							Tag:  `validate:"required"`,
+						},
 					},
-					{
-						Name: "MyAge",
-						Type: "uint8",
-						Tag:  "validate:\"required\"",
-					},
+					HasValidateTag: true,
+					PackageName:    "main",
 				},
-				HasValidateTag: true,
 			},
-			want: `func UserValidate(u *User) []error {
+			want: `package main
+
+import (
+	"fmt"
+)
+
+func UserValidate(obj *User) []error {
 	var errs []error
 
-	if u.FirstName == "" {
+	if obj.FirstName == "" {
 		errs = append(errs, fmt.Errorf("%w: FirstName required", ErrValidation))
 	}
 
-	if u.MyAge == 0 {
+	if obj.MyAge == 0 {
 		errs = append(errs, fmt.Errorf("%w: MyAge required", ErrValidation))
 	}
 
@@ -53,24 +60,21 @@ func TestFuncValidatorGenerate(t *testing.T) {
 			wantErr: false,
 		},
 	}
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			s := &StructInfo{
-				Name:           tt.fields.Name,
-				FieldsInfo:     tt.fields.FieldsValidations,
-				HasValidateTag: tt.fields.HasValidateTag,
-			}
-			got, err := s.GenerateFuncValidator()
+			fv := tt.fields.StructInfo
+			got, err := fv.GenerateValidator()
 			if (err != nil) != tt.wantErr {
-				t.Errorf("FuncValidator.Generate() error = %v, wantErr %v", err, tt.wantErr)
+				t.Errorf("FileValidator.Generate() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
 			if got != tt.want {
-				t.Errorf("FuncValidator.Generate() = %v, want %v", got, tt.want)
+				t.Errorf("FileValidator.Generate() = %v, want %v", got, tt.want)
 				dmp := diffmatchpatch.New()
 				diffs := dmp.DiffMain(tt.want, got, false)
 				if len(diffs) > 1 {
-					t.Errorf("FuncValidator.Generate() diff = \n%v", dmp.DiffPrettyText(diffs))
+					t.Errorf("FileValidator.Generate() diff = \n%v", dmp.DiffPrettyText(diffs))
 				}
 			}
 		})
