deafc5e0dba9a1d7f4a77748df94b7f1cd5bc296
Alex S. Garzão <alexgarzao@gmail.com>
2024-11-23 20:14:48 -0300

    feat: lte and gte validations to uint8 type
    With lte and gte, was necessary to parse validate tag to correct
    split validations.

--- struct_info_test.go
+++ struct_info_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/sergi/go-diff/diffmatchpatch"
@@ -23,14 +24,16 @@ func TestStructInfoGenerateValidator(t *testing.T) {
 					Name: "User",
 					FieldsInfo: []FieldInfo{
 						{
-							Name: "FirstName",
-							Type: "string",
-							Tag:  `validate:"required"`,
+							Name:        "FirstName",
+							Type:        "string",
+							Tag:         `validate:"required"`,
+							Validations: []string{"required"},
 						},
 						{
-							Name: "MyAge",
-							Type: "uint8",
-							Tag:  `validate:"required"`,
+							Name:        "MyAge",
+							Type:        "uint8",
+							Tag:         `validate:"required"`,
+							Validations: []string{"required"},
 						},
 					},
 					HasValidateTag: true,
@@ -76,6 +79,84 @@ func UserValidate(obj *User) []error {
 				if len(diffs) > 1 {
 					t.Errorf("FileValidator.Generate() diff = \n%v", dmp.DiffPrettyText(diffs))
 				}
+			}
+		})
+	}
+}
+
+func TestGetFieldTestElements(t *testing.T) {
+	type args struct {
+		fieldValidation string
+		fieldType       string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    FieldTestElements
+		wantErr bool
+	}{
+		{
+			name: "Required string",
+			args: args{
+				fieldValidation: "required",
+				fieldType:       "string",
+			},
+			want: FieldTestElements{
+				operator:     "==",
+				operand:      `""`,
+				errorMessage: "%s required",
+			},
+			wantErr: false,
+		},
+		{
+			name: "Required uint8",
+			args: args{
+				fieldValidation: "required",
+				fieldType:       "uint8",
+			},
+			want: FieldTestElements{
+				operator:     "==",
+				operand:      `0`,
+				errorMessage: "%s required",
+			},
+			wantErr: false,
+		},
+		{
+			name: "uint8 >= 0",
+			args: args{
+				fieldValidation: "gte=0",
+				fieldType:       "uint8",
+			},
+			want: FieldTestElements{
+				operator:     "<",
+				operand:      `0`,
+				errorMessage: "%s must be >= 0",
+			},
+			wantErr: false,
+		},
+		{
+			name: "uint8 <= 130",
+			args: args{
+				fieldValidation: "lte=130",
+				fieldType:       "uint8",
+			},
+			want: FieldTestElements{
+				operator:     ">",
+				operand:      `130`,
+				errorMessage: "%s must be <= 130",
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := GetFieldTestElements(tt.args.fieldValidation, tt.args.fieldType)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GetFieldTestElements() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("GetFieldTestElements() = %v, want %v", got, tt.want)
 			}
 		})
 	}
