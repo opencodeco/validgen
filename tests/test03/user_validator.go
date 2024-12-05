37a28c523a8b26135ec0a09e15e5e582f1fddcb6
Alex S. Garzão <alexgarzao@gmail.com>
2024-12-05 20:24:28 -0300

    test: validating lte/gte string and expected code

---
+++ tests/test03/user_validator.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+)
+
+func UserValidate(obj *User) []error {
+	var errs []error
+
+	if obj.FirstName == "" {
+		errs = append(errs, fmt.Errorf("%w: FirstName required", ErrValidation))
+	}
+
+	if obj.LastName == "" {
+		errs = append(errs, fmt.Errorf("%w: LastName required", ErrValidation))
+	}
+
+	if obj.Age < 0 {
+		errs = append(errs, fmt.Errorf("%w: Age must be >= 0", ErrValidation))
+	}
+
+	if obj.Age > 130 {
+		errs = append(errs, fmt.Errorf("%w: Age must be <= 130", ErrValidation))
+	}
+
+	if len(obj.UserName) < 5 {
+		errs = append(errs, fmt.Errorf("%w: length UserName must be >= 5", ErrValidation))
+	}
+
+	if len(obj.UserName) > 10 {
+		errs = append(errs, fmt.Errorf("%w: length UserName must be <= 10", ErrValidation))
+	}
+
+	return errs
+}
