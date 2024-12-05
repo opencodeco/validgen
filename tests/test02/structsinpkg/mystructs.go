4f1f1054181e709a7a64b17e589298004777fbf2
Alex S. Garzão <alexgarzao@gmail.com>
2024-12-05 20:23:35 -0300

    tests: better error report and validations

--- tests/test02/structsinpkg/mystructs.go
+++ tests/test02/structsinpkg/mystructs.go
@@ -4,8 +4,6 @@ type User struct {
 	FirstName string `validate:"required"`
 	LastName  string `validate:"required"`
 	Age       uint8  `validate:"required"`
-	// Age       uint8  `validate:"gte=0,lte=130"`
-	// Optional  string
 }
 
 type NoValidateInfo struct {
--- tests/test02/test02.go
+++ tests/test02/test02.go
@@ -7,10 +7,22 @@ import (
 )
 
 func main() {
-	u := &structsinpkg.User{}
-	if err := structsinpkg.UserValidate(u); err != nil {
-		fmt.Printf("User: %+v Error: %s\n", u, err)
+	u1 := &structsinpkg.User{}
+	if err := structsinpkg.UserValidate(u1); err != nil {
+		fmt.Printf("User: %+v Error: %s\n", u1, err)
 	} else {
-		fmt.Printf("User: %+v is valid\n", u)
+		fmt.Printf("User: %+v is valid\n", u1)
+	}
+
+	u2 := &structsinpkg.User{
+		FirstName: "First",
+		LastName:  "Last",
+		Age:       1,
+	}
+
+	if err := structsinpkg.UserValidate(u2); err != nil {
+		fmt.Printf("User: %+v Error: %s\n", u2, err)
+	} else {
+		fmt.Printf("User: %+v is valid\n", u2)
 	}
 }
