8a235309240d6d1f591ac5c23a85d59b4ac1ee42
Alex S. Garzão <alexgarzao@gmail.com>
2024-12-11 20:54:54 -0300

    tests: updating expected generated code

--- tests/test02/structsinpkg/user_validator.go
+++ tests/test02/structsinpkg/user_validator.go
@@ -7,15 +7,15 @@ import (
 func UserValidate(obj *User) []error {
 	var errs []error
 
-	if obj.FirstName == "" {
+	if !(obj.FirstName != "") {
 		errs = append(errs, fmt.Errorf("%w: FirstName required", ErrValidation))
 	}
 
-	if obj.LastName == "" {
+	if !(obj.LastName != "") {
 		errs = append(errs, fmt.Errorf("%w: LastName required", ErrValidation))
 	}
 
-	if obj.Age == 0 {
+	if !(obj.Age != 0) {
 		errs = append(errs, fmt.Errorf("%w: Age required", ErrValidation))
 	}
 
