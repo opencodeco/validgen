10757e4f04ebfa302aecc436bbdb327c4de56e3b
Alex S. Garzão <alexgarzao@gmail.com>
2024-12-11 20:54:54 -0300

    tests: updating expected generated code

--- tests/test03/user_validator.go
+++ tests/test03/user_validator.go
@@ -7,27 +7,27 @@ import (
 func UserValidate(obj *User) []error {
 	var errs []error
 
-	if obj.FirstName == "" {
+	if !(obj.FirstName != "") {
 		errs = append(errs, fmt.Errorf("%w: FirstName required", ErrValidation))
 	}
 
-	if obj.LastName == "" {
+	if !(obj.LastName != "") {
 		errs = append(errs, fmt.Errorf("%w: LastName required", ErrValidation))
 	}
 
-	if obj.Age < 0 {
+	if !(obj.Age >= 0) {
 		errs = append(errs, fmt.Errorf("%w: Age must be >= 0", ErrValidation))
 	}
 
-	if obj.Age > 130 {
+	if !(obj.Age <= 130) {
 		errs = append(errs, fmt.Errorf("%w: Age must be <= 130", ErrValidation))
 	}
 
-	if len(obj.UserName) < 5 {
+	if !(len(obj.UserName) >= 5) {
 		errs = append(errs, fmt.Errorf("%w: length UserName must be >= 5", ErrValidation))
 	}
 
-	if len(obj.UserName) > 10 {
+	if !(len(obj.UserName) <= 10) {
 		errs = append(errs, fmt.Errorf("%w: length UserName must be <= 10", ErrValidation))
 	}
 
