383d6767c28d5069ef5c6e3ff59fc1c13fc46c04
Alex S. Garzão <alexgarzao@gmail.com>
2024-12-13 15:55:07 -0300

    tests: using package types to import ErrValidation

--- tests/test03/user_validator.go
+++ tests/test03/user_validator.go
@@ -2,33 +2,35 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/opencodeco/myvalidator/types"
 )
 
 func UserValidate(obj *User) []error {
 	var errs []error
 
 	if !(obj.FirstName != "") {
-		errs = append(errs, fmt.Errorf("%w: FirstName required", ErrValidation))
+		errs = append(errs, fmt.Errorf("%w: FirstName required", types.ErrValidation))
 	}
 
 	if !(obj.LastName != "") {
-		errs = append(errs, fmt.Errorf("%w: LastName required", ErrValidation))
+		errs = append(errs, fmt.Errorf("%w: LastName required", types.ErrValidation))
 	}
 
 	if !(obj.Age >= 0) {
-		errs = append(errs, fmt.Errorf("%w: Age must be >= 0", ErrValidation))
+		errs = append(errs, fmt.Errorf("%w: Age must be >= 0", types.ErrValidation))
 	}
 
 	if !(obj.Age <= 130) {
-		errs = append(errs, fmt.Errorf("%w: Age must be <= 130", ErrValidation))
+		errs = append(errs, fmt.Errorf("%w: Age must be <= 130", types.ErrValidation))
 	}
 
 	if !(len(obj.UserName) >= 5) {
-		errs = append(errs, fmt.Errorf("%w: length UserName must be >= 5", ErrValidation))
+		errs = append(errs, fmt.Errorf("%w: length UserName must be >= 5", types.ErrValidation))
 	}
 
 	if !(len(obj.UserName) <= 10) {
-		errs = append(errs, fmt.Errorf("%w: length UserName must be <= 10", ErrValidation))
+		errs = append(errs, fmt.Errorf("%w: length UserName must be <= 10", types.ErrValidation))
 	}
 
 	return errs
