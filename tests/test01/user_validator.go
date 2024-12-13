590baffd03295ce5629c02c8bfcc88a783f4f773
Alex S. Garzão <alexgarzao@gmail.com>
2024-12-13 15:55:07 -0300

    tests: using package types to import ErrValidation

--- tests/test01/user_validator.go
+++ tests/test01/user_validator.go
@@ -2,21 +2,23 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/opencodeco/myvalidator/types"
 )
 
 func UserValidate(obj *User) []error {
 	var errs []error
 
 	if !(obj.FirstName != "") {
-		errs = append(errs, fmt.Errorf("%w: FirstName required", ErrValidation))
+		errs = append(errs, fmt.Errorf("%w: FirstName required", types.ErrValidation))
 	}
 
 	if !(obj.LastName != "") {
-		errs = append(errs, fmt.Errorf("%w: LastName required", ErrValidation))
+		errs = append(errs, fmt.Errorf("%w: LastName required", types.ErrValidation))
 	}
 
 	if !(obj.Age != 0) {
-		errs = append(errs, fmt.Errorf("%w: Age required", ErrValidation))
+		errs = append(errs, fmt.Errorf("%w: Age required", types.ErrValidation))
 	}
 
 	return errs
