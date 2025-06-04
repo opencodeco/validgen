5a19d5c33823ffe072d4462a6b0f586d0af581c6
Alex S. Garzão <alexgarzao@gmail.com>
2025-06-04 10:13:51 -0300

    feat: validate struct using go validator

---
+++ benchtests/validgen_validator.go
@@ -0,0 +1,37 @@
+package benchtests
+
+import (
+	"fmt"
+
+	"github.com/opencodeco/validgen/types"
+)
+
+func ValidGenValidate(obj *StructToValidate) []error {
+	var errs []error
+
+	if !(obj.FirstName != "") {
+		errs = append(errs, fmt.Errorf("%w: FirstName required", types.ErrValidation))
+	}
+
+	if !(obj.LastName != "") {
+		errs = append(errs, fmt.Errorf("%w: LastName required", types.ErrValidation))
+	}
+
+	if !(obj.Age >= 0) {
+		errs = append(errs, fmt.Errorf("%w: Age must be >= 0", types.ErrValidation))
+	}
+
+	if !(obj.Age <= 130) {
+		errs = append(errs, fmt.Errorf("%w: Age must be <= 130", types.ErrValidation))
+	}
+
+	if !(len(obj.UserName) >= 5) {
+		errs = append(errs, fmt.Errorf("%w: length UserName must be >= 5", types.ErrValidation))
+	}
+
+	if !(len(obj.UserName) <= 10) {
+		errs = append(errs, fmt.Errorf("%w: length UserName must be <= 10", types.ErrValidation))
+	}
+
+	return errs
+}
