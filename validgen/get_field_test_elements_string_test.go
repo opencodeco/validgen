c5c2588aa7169f8aded8b335c34d226c8f2ef01a
Alex S. Garzão <alexgarzao@gmail.com>
2025-06-24 20:09:37 -0300

    refact: split between string and numeric tests

---
+++ validgen/get_field_test_elements_string_test.go
@@ -0,0 +1,78 @@
+package validgen
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestGetFieldTestElementsWithStringFields(t *testing.T) {
+	type args struct {
+		fieldName       string
+		fieldValidation string
+		fieldType       string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    FieldTestElements
+		wantErr bool
+	}{
+		{
+			name: "Required string",
+			args: args{
+				fieldName:       "myfield1",
+				fieldValidation: "required",
+				fieldType:       "string",
+			},
+			want: FieldTestElements{
+				loperand:     "obj.myfield1",
+				operator:     "!=",
+				roperand:     `""`,
+				errorMessage: "myfield1 required",
+			},
+			wantErr: false,
+		},
+		{
+			name: "String size >= 5",
+			args: args{
+				fieldName:       "myfield5",
+				fieldValidation: "gte=5",
+				fieldType:       "string",
+			},
+			want: FieldTestElements{
+				loperand:     "len(obj.myfield5)",
+				operator:     ">=",
+				roperand:     `5`,
+				errorMessage: "length myfield5 must be >= 5",
+			},
+			wantErr: false,
+		},
+		{
+			name: "String size <= 10",
+			args: args{
+				fieldName:       "myfield6",
+				fieldValidation: "lte=10",
+				fieldType:       "string",
+			},
+			want: FieldTestElements{
+				loperand:     "len(obj.myfield6)",
+				operator:     "<=",
+				roperand:     `10`,
+				errorMessage: "length myfield6 must be <= 10",
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := GetFieldTestElements(tt.args.fieldName, tt.args.fieldValidation, tt.args.fieldType)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GetFieldTestElements() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("GetFieldTestElements() = %+v, want %+v", got, tt.want)
+			}
+		})
+	}
+}
