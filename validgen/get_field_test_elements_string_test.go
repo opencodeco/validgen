fd0d4f3928672c22af6baf704b69ed2c62d83e32
Alex S. Garzão <alexgarzao@gmail.com>
2025-07-03 19:47:39 -0300

    refact: define type and wanterr in strings tests is useless

--- validgen/get_field_test_elements_string_test.go
+++ validgen/get_field_test_elements_string_test.go
@@ -9,20 +9,17 @@ func TestGetFieldTestElementsWithStringFields(t *testing.T) {
 	type args struct {
 		fieldName       string
 		fieldValidation string
-		fieldType       string
 	}
 	tests := []struct {
-		name    string
-		args    args
-		want    FieldTestElements
-		wantErr bool
+		name string
+		args args
+		want FieldTestElements
 	}{
 		{
 			name: "Equal string",
 			args: args{
 				fieldName:       "myfield1",
 				fieldValidation: "eq=abc",
-				fieldType:       "string",
 			},
 			want: FieldTestElements{
 				loperand:     "obj.myfield1",
@@ -30,14 +27,12 @@ func TestGetFieldTestElementsWithStringFields(t *testing.T) {
 				roperand:     `"abc"`,
 				errorMessage: "myfield1 must be equal to 'abc'",
 			},
-			wantErr: false,
 		},
 		{
 			name: "Required string",
 			args: args{
 				fieldName:       "myfield1",
 				fieldValidation: "required",
-				fieldType:       "string",
 			},
 			want: FieldTestElements{
 				loperand:     "obj.myfield1",
@@ -45,14 +40,12 @@ func TestGetFieldTestElementsWithStringFields(t *testing.T) {
 				roperand:     `""`,
 				errorMessage: "myfield1 required",
 			},
-			wantErr: false,
 		},
 		{
 			name: "String size >= 5",
 			args: args{
 				fieldName:       "myfield5",
 				fieldValidation: "min=5",
-				fieldType:       "string",
 			},
 			want: FieldTestElements{
 				loperand:     "len(obj.myfield5)",
@@ -60,14 +53,12 @@ func TestGetFieldTestElementsWithStringFields(t *testing.T) {
 				roperand:     `5`,
 				errorMessage: "myfield5 length must be >= 5",
 			},
-			wantErr: false,
 		},
 		{
 			name: "String size <= 10",
 			args: args{
 				fieldName:       "myfield6",
 				fieldValidation: "max=10",
-				fieldType:       "string",
 			},
 			want: FieldTestElements{
 				loperand:     "len(obj.myfield6)",
@@ -75,14 +66,12 @@ func TestGetFieldTestElementsWithStringFields(t *testing.T) {
 				roperand:     `10`,
 				errorMessage: "myfield6 length must be <= 10",
 			},
-			wantErr: false,
 		},
 		{
 			name: "Equal string ignore case",
 			args: args{
 				fieldName:       "myStrField",
 				fieldValidation: "eq_ignore_case=AbC",
-				fieldType:       "string",
 			},
 			want: FieldTestElements{
 				loperand:     "types.ToLower(obj.myStrField)",
@@ -90,13 +79,11 @@ func TestGetFieldTestElementsWithStringFields(t *testing.T) {
 				roperand:     `"abc"`,
 				errorMessage: "myStrField must be equal to 'abc'",
 			},
-			wantErr: false,
 		},
 		{
 			name: "Len string",
 			args: args{
 				fieldName:       "myStrField",
-				fieldType:       "string",
 				fieldValidation: "len=8",
 			},
 			want: FieldTestElements{
@@ -105,13 +92,11 @@ func TestGetFieldTestElementsWithStringFields(t *testing.T) {
 				roperand:     `8`,
 				errorMessage: "myStrField length must be 8",
 			},
-			wantErr: false,
 		},
 		{
 			name: "Not equal string",
 			args: args{
 				fieldName:       "MyFieldNotEqual",
-				fieldType:       "string",
 				fieldValidation: "neq=abc",
 			},
 			want: FieldTestElements{
@@ -120,14 +105,12 @@ func TestGetFieldTestElementsWithStringFields(t *testing.T) {
 				roperand:     `"abc"`,
 				errorMessage: "MyFieldNotEqual must be not equal to 'abc'",
 			},
-			wantErr: false,
 		},
 		{
 			name: "Not equal string ignore case",
 			args: args{
 				fieldName:       "MyFieldNotEqual",
 				fieldValidation: "neq_ignore_case=AbC",
-				fieldType:       "string",
 			},
 			want: FieldTestElements{
 				loperand:     "types.ToLower(obj.MyFieldNotEqual)",
@@ -135,15 +118,16 @@ func TestGetFieldTestElementsWithStringFields(t *testing.T) {
 				roperand:     `"abc"`,
 				errorMessage: "MyFieldNotEqual must be not equal to 'abc'",
 			},
-			wantErr: false,
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := GetFieldTestElements(tt.args.fieldName, tt.args.fieldValidation, tt.args.fieldType)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("GetFieldTestElements() error = %v, wantErr %v", err, tt.wantErr)
+			fieldType := "string"
+			wantErr := false
+			got, err := GetFieldTestElements(tt.args.fieldName, tt.args.fieldValidation, fieldType)
+			if (err != nil) != false {
+				t.Errorf("GetFieldTestElements() error = %v, wantErr %v", err, wantErr)
 				return
 			}
 			if !reflect.DeepEqual(got, tt.want) {
