bce91d631ed16ebf12335fb9085d6cef93108796
Alex S. Garzão <alexgarzao@gmail.com>
2025-07-08 20:31:10 -0300

    refact: change from lte/gte to min/max

--- tests/bench/struct_to_validate.go
+++ tests/bench/struct_to_validate.go
@@ -4,6 +4,6 @@ type StructToValidate struct {
 	FirstName string `validate:"required"`
 	LastName  string `validate:"required"`
 	Age       uint8  `validate:"gte=18,lte=130"`
-	UserName  string `validate:"gte=5,lte=10"`
+	UserName  string `validate:"min=5,max=10"`
 	Optional  string
 }
