2b319c9b9a2d21005c02581047a62c3d8056b969
Alex S. Garzão <alexgarzao@gmail.com>
2025-07-11 20:26:32 -0300

    refact: code to generate if in if_code.go

---
+++ validgen/if_code.go
@@ -0,0 +1,34 @@
+package validgen
+
+import (
+	"fmt"
+)
+
+func condition(fieldName, fieldType string, fieldValidations []string) (string, error) {
+
+	tests := ""
+	for _, fieldValidation := range fieldValidations {
+		testCode, err := IfCode(fieldName, fieldValidation, fieldType)
+		if err != nil {
+			return "", err
+		}
+
+		tests += testCode
+	}
+
+	return tests, nil
+}
+
+func IfCode(fieldName, fieldValidation, fieldType string) (string, error) {
+	testElements, err := GetTestElements(fieldName, fieldValidation, fieldType)
+	if err != nil {
+		return "", fmt.Errorf("field %s: %w", fieldName, err)
+	}
+
+	return fmt.Sprintf(
+		`
+	if !(%s %s %s) {
+		errs = append(errs, types.NewValidationError("%s"))
+	}
+`, testElements.loperand, testElements.operator, testElements.roperand, testElements.errorMessage), nil
+}
--- validgen/struct_info.go
+++ validgen/struct_info.go
@@ -56,35 +56,6 @@ func (fv *StructInfo) GenerateValidator() (string, error) {
 	return code.String(), nil
 }
 
-func condition(fieldName, fieldType string, fieldValidations []string) (string, error) {
-
-	tests := ""
-	for _, fieldValidation := range fieldValidations {
-		testCode, err := IfCode(fieldName, fieldValidation, fieldType)
-		if err != nil {
-			return "", err
-		}
-
-		tests += testCode
-	}
-
-	return tests, nil
-}
-
-func IfCode(fieldName, fieldValidation, fieldType string) (string, error) {
-	testElements, err := GetTestElements(fieldName, fieldValidation, fieldType)
-	if err != nil {
-		return "", fmt.Errorf("field %s: %w", fieldName, err)
-	}
-
-	return fmt.Sprintf(
-		`
-	if !(%s %s %s) {
-		errs = append(errs, types.NewValidationError("%s"))
-	}
-`, testElements.loperand, testElements.operator, testElements.roperand, testElements.errorMessage), nil
-}
-
 func (s *StructInfo) GenerateFileValidator() error {
 	fmt.Printf("Generating struct %s validations code\n", s.Name)
 
