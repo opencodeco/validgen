55a1b9e5c7d8b11bb529f85fa0221f44a2925516
Alex S. Garzão <alexgarzao@gmail.com>
2025-07-11 21:38:40 -0300

    chore: update error message

--- tests/endtoend/stringtype_validator.go
+++ tests/endtoend/stringtype_validator.go
@@ -34,11 +34,11 @@ func StringTypeValidate(obj *StringType) []error {
 	}
 
 	if !(obj.FieldNeq != "cba") {
-		errs = append(errs, types.NewValidationError("FieldNeq must be not equal to 'cba'"))
+		errs = append(errs, types.NewValidationError("FieldNeq must not be equal to 'cba'"))
 	}
 
 	if !(types.ToLower(obj.FieldNeqIC) != "yes") {
-		errs = append(errs, types.NewValidationError("FieldNeqIC must be not equal to 'yes'"))
+		errs = append(errs, types.NewValidationError("FieldNeqIC must not be equal to 'yes'"))
 	}
 
 	return errs
