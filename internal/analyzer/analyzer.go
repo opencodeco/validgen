8dbc84057fddd49d01630591ff0af91de5c906f2
Alex S. Garzão <alexgarzao@gmail.com>
2025-08-28 17:08:47 -0300

    feat: analyzer now checks for field operations (eqfield, neqfield, etc)

--- internal/analyzer/analyzer.go
+++ internal/analyzer/analyzer.go
@@ -12,6 +12,21 @@ import (
 const validTag = "valid"
 
 func AnalyzeStructs(structs []*parser.Struct) ([]*Struct, error) {
+	result, err := analyzeFieldValidations(structs)
+	if err != nil {
+		return nil, err
+	}
+
+	err = analyzeFieldOperations(result)
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
+
+func analyzeFieldValidations(structs []*parser.Struct) ([]*Struct, error) {
+
 	result := []*Struct{}
 
 	for _, st := range structs {
@@ -55,4 +70,62 @@ func parseFieldValidations(fieldTag string) ([]string, bool) {
 	}
 
 	return fieldValidations, hasValidTag
+}
+
+func analyzeFieldOperations(structs []*Struct) error {
+
+	fieldOperations := map[string]struct{}{
+		"eqfield":  {},
+		"neqfield": {},
+		"gtefield": {},
+		"gtfield":  {},
+		"ltefield": {},
+		"ltfield":  {},
+	}
+
+	// For string type, eqfield and nefield.
+	// For uint8 type, eqfield, gtefield, gtfield, ltefield, ltfield and nefield.
+	fieldOperationsByType := map[string]struct{}{
+		"string,eqfield":  {},
+		"string,neqfield": {},
+	}
+
+	for _, st := range structs {
+		fieldsType := map[string]string{}
+		for _, fd := range st.Fields {
+			fieldsType[fd.FieldName] = fd.Type
+		}
+
+		for i, fd := range st.Fields {
+			for _, val := range st.FieldsValidations[i].Validations {
+				// Check if is a field operation.
+				op := val.Operation
+				if _, ok := fieldOperations[op]; !ok {
+					continue
+				}
+
+				// Check if is a valid operation for a type.
+				fd1Type := fd.Type
+				if _, ok := fieldOperationsByType[fd1Type+","+op]; !ok {
+					return types.NewValidationError("invalid operation %s to %s type", op, fd1Type)
+				}
+
+				fd1Name := fd.FieldName
+				fd2Name := val.Values[0]
+
+				// Check if field exits.
+				f2Type, ok := fieldsType[fd2Name]
+				if !ok {
+					return types.NewValidationError("operation %s: undefined field %s", op, fd2Name)
+				}
+
+				// Check if fields has the same type.
+				if fd.Type != f2Type {
+					return types.NewValidationError("operation %s: mismatched types between %s and %s", op, fd1Name, fd2Name)
+				}
+			}
+		}
+	}
+
+	return nil
 }
---
+++ internal/analyzer/analyzer_test.go
@@ -0,0 +1,129 @@
+package analyzer
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/opencodeco/validgen/internal/parser"
+	"github.com/opencodeco/validgen/types"
+)
+
+func TestAnalyzeStructsWithValidFieldOperations(t *testing.T) {
+	tests := []struct {
+		name  string
+		fType string
+		op    string
+	}{
+		{
+			name:  "valid eqfield between strings",
+			fType: "string",
+			op:    "eqfield",
+		},
+		{
+			name:  "valid neqfield between strings",
+			fType: "string",
+			op:    "neqfield",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			arg := []*parser.Struct{
+				{
+					Fields: []parser.Field{
+						{
+							FieldName: "Field1",
+							Type:      tt.fType,
+							Tag:       fmt.Sprintf(`valid:"%s=Field2"`, tt.op),
+						},
+						{
+							FieldName: "Field2",
+							Type:      tt.fType,
+							Tag:       ``,
+						},
+					},
+				},
+			}
+
+			_, err := AnalyzeStructs(arg)
+			if err != nil {
+				t.Errorf("AnalyzeStructs() error = %v, wantErr %v", err, nil)
+				return
+			}
+		})
+	}
+}
+
+func TestAnalyzeStructsWithInvalidFieldOperations(t *testing.T) {
+	tests := []struct {
+		name    string
+		arg     []*parser.Struct
+		wantErr error
+	}{
+		{
+			name: "mismatched types between fields",
+			arg: []*parser.Struct{
+				{
+					Fields: []parser.Field{
+						{
+							FieldName: "Field1",
+							Type:      "string",
+							Tag:       `valid:"eqfield=Field2"`,
+						},
+						{
+							FieldName: "Field2",
+							Type:      "uint8",
+							Tag:       ``,
+						},
+					},
+				},
+			},
+			wantErr: types.NewValidationError("operation eqfield: mismatched types between Field1 and Field2"),
+		},
+		{
+			name: "undefined field",
+			arg: []*parser.Struct{
+				{
+					Fields: []parser.Field{
+						{
+							FieldName: "Field1",
+							Type:      "string",
+							Tag:       `valid:"eqfield=Field2"`,
+						},
+					},
+				},
+			},
+			wantErr: types.NewValidationError("operation eqfield: undefined field Field2"),
+		},
+		{
+			name: "invalid operation to string type",
+			arg: []*parser.Struct{
+				{
+					Fields: []parser.Field{
+						{
+							FieldName: "Field1",
+							Type:      "string",
+							Tag:       `valid:"ltfield=Field2"`,
+						},
+						{
+							FieldName: "Field2",
+							Type:      "uint8",
+							Tag:       ``,
+						},
+					},
+				},
+			},
+			wantErr: types.NewValidationError("invalid operation ltfield to string type"),
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := AnalyzeStructs(tt.arg)
+			if err != tt.wantErr {
+				t.Errorf("AnalyzeStructs() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+		})
+	}
+}
