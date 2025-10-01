c0a76e65d0d1004ab5309a85e2511fb735271094
Alex S. Garzão <alexgarzao@gmail.com>
2025-10-01 18:28:07 -0300

    feat: normalize the specific type to a class (int, float, string, bool)

--- internal/common/types.go
+++ internal/common/types.go
@@ -1,5 +1,7 @@
 package common
 
+import "strings"
+
 type FieldType struct {
 	ComposedType string // array ([N]), map (map) or slice ([])
 	BaseType     string // base type (e.g. string, int, etc.)
@@ -12,20 +14,20 @@ func (ft FieldType) IsGoType() bool {
 	}
 
 	goTypes := map[string]struct{}{
-		"string": {},
-		"bool":   {},
-		// "int":        {},
-		// "int8":       {},
-		// "int16":      {},
-		// "int32":      {},
-		// "int64":      {},
-		// "uint":       {},
-		"uint8": {},
-		// "uint16":     {},
-		// "uint32":     {},
-		// "uint64":     {},
-		// "float32":    {},
-		// "float64":    {},
+		"string":  {},
+		"bool":    {},
+		"int":     {},
+		"int8":    {},
+		"int16":   {},
+		"int32":   {},
+		"int64":   {},
+		"uint":    {},
+		"uint8":   {},
+		"uint16":  {},
+		"uint32":  {},
+		"uint64":  {},
+		"float32": {},
+		"float64": {},
 		// "complex64":  {},
 		// "complex128": {},
 	}
@@ -33,6 +35,31 @@ func (ft FieldType) IsGoType() bool {
 	_, ok := goTypes[ft.BaseType]
 
 	return ok
+}
+
+func (ft FieldType) NormalizeBaseType() NormalizedBaseType {
+	// Base type grouping by type (e.g. string, bool, int and float)
+
+	normalizedBaseType := map[string]NormalizedBaseType{
+		"string":  StringType,
+		"bool":    BoolType,
+		"int":     IntType,
+		"int8":    IntType,
+		"int16":   IntType,
+		"int32":   IntType,
+		"int64":   IntType,
+		"uint":    IntType,
+		"uint8":   IntType,
+		"uint16":  IntType,
+		"uint32":  IntType,
+		"uint64":  IntType,
+		"float32": FloatType,
+		"float64": FloatType,
+		// "complex64":  ComplexType,
+		// "complex128": ComplexType,
+	}
+
+	return normalizedBaseType[ft.BaseType]
 }
 
 func (ft FieldType) ToString() string {
@@ -46,4 +73,77 @@ func (ft FieldType) ToString() string {
 	}
 
 	return ft.BaseType
+}
+
+func (ft FieldType) ToNormalizedString() string {
+	switch ft.ComposedType {
+	case "[N]":
+		return "[N]" + ft.NormalizeBaseType().String()
+	case "[]":
+		return "[]" + ft.NormalizeBaseType().String()
+	case "map":
+		return "map[" + ft.NormalizeBaseType().String() + "]"
+	}
+
+	return ft.NormalizeBaseType().String()
+}
+
+func FromNormalizedToBasicTypes(t string) []string {
+	switch t {
+	case "<STRING>":
+		return []string{"string"}
+	case "<BOOL>":
+		return []string{"bool"}
+	case "<INT>":
+		return []string{"int", "int8", "int16", "int32", "int64", "uint", "uint8", "uint16", "uint32", "uint64"}
+	case "<FLOAT>":
+		return []string{"float32", "float64"}
+	case "map[<STRING>]":
+		return []string{"map[string]"}
+	case "map[<BOOL>]":
+		return []string{"map[bool]"}
+	case "map[<INT>]":
+		return []string{"map[int]", "map[int8]", "map[int16]", "map[int32]", "map[int64]", "map[uint]", "map[uint8]", "map[uint16]", "map[uint32]", "map[uint64]"}
+	case "map[<FLOAT>]":
+		return []string{"map[float32]", "map[float64]"}
+	case "[]<STRING>":
+		return []string{"[]string"}
+	case "[]<BOOL>":
+		return []string{"[]bool"}
+	case "[]<INT>":
+		return []string{"[]int", "[]int8", "[]int16", "[]int32", "[]int64", "[]uint", "[]uint8", "[]uint16", "[]uint32", "[]uint64"}
+	case "[]<FLOAT>":
+		return []string{"[]float32", "[]float64"}
+	}
+
+	// Try to remove [N]
+	if t[0] == '[' {
+		closeBracketIndex := strings.Index(t, "]")
+		if closeBracketIndex == -1 {
+			return []string{"invalid"}
+		}
+
+		size := t[1:closeBracketIndex]
+		basicType := t[closeBracketIndex+1:]
+		sizeInType := "[" + size + "]"
+		result := []string{}
+		switch basicType {
+		case "<STRING>":
+			result = []string{"[N]string"}
+		case "<BOOL>":
+			result = []string{"[N]bool"}
+		case "<INT>":
+			result = []string{"[N]int", "[N]int8", "[N]int16", "[N]int32", "[N]int64", "[N]uint", "[N]uint8", "[N]uint16", "[N]uint32", "[N]uint64"}
+		case "<FLOAT>":
+			result = []string{"[N]float32", "[N]float64"}
+		}
+
+		for i := range result {
+			result[i] = strings.ReplaceAll(result[i], "[N]", sizeInType)
+		}
+
+		return result
+	}
+
+	return []string{"invalid"}
 }
--- internal/common/types_test.go
+++ internal/common/types_test.go
@@ -1,6 +1,9 @@
 package common
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestFieldType_ToString(t *testing.T) {
 	type fields struct {
@@ -128,6 +131,111 @@ func TestFieldType_IsGoType(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := tt.args.fieldType.IsGoType(); got != tt.want {
 				t.Errorf("IsGoType() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestFromNormalizedToBasicTypes(t *testing.T) {
+	type args struct {
+		t string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		{
+			name: "string type",
+			args: args{t: "<STRING>"},
+			want: []string{"string"},
+		},
+		{
+			name: "bool type",
+			args: args{t: "<BOOL>"},
+			want: []string{"bool"},
+		},
+		{
+			name: "int type",
+			args: args{t: "<INT>"},
+			want: []string{"int", "int8", "int16", "int32", "int64", "uint", "uint8", "uint16", "uint32", "uint64"},
+		},
+		{
+			name: "float type",
+			args: args{t: "<FLOAT>"},
+			want: []string{"float32", "float64"},
+		},
+		{
+			name: "map string type",
+			args: args{t: "map[<STRING>]"},
+			want: []string{"map[string]"},
+		},
+		{
+			name: "map bool type",
+			args: args{t: "map[<BOOL>]"},
+			want: []string{"map[bool]"},
+		},
+		{
+			name: "map int type",
+			args: args{t: "map[<INT>]"},
+			want: []string{"map[int]", "map[int8]", "map[int16]", "map[int32]", "map[int64]", "map[uint]", "map[uint8]", "map[uint16]", "map[uint32]", "map[uint64]"},
+		},
+		{
+			name: "map float type",
+			args: args{t: "map[<FLOAT>]"},
+			want: []string{"map[float32]", "map[float64]"},
+		},
+		{
+			name: "slice string type",
+			args: args{t: "[]<STRING>"},
+			want: []string{"[]string"},
+		},
+		{
+			name: "slice bool type",
+			args: args{t: "[]<BOOL>"},
+			want: []string{"[]bool"},
+		},
+		{
+			name: "slice int type",
+			args: args{t: "[]<INT>"},
+			want: []string{"[]int", "[]int8", "[]int16", "[]int32", "[]int64", "[]uint", "[]uint8", "[]uint16", "[]uint32", "[]uint64"},
+		},
+		{
+			name: "slice float type",
+			args: args{t: "[]<FLOAT>"},
+			want: []string{"[]float32", "[]float64"},
+		},
+		{
+			name: "array string type",
+			args: args{t: "[3]<STRING>"},
+			want: []string{"[3]string"},
+		},
+		{
+			name: "array bool type",
+			args: args{t: "[3]<BOOL>"},
+			want: []string{"[3]bool"},
+		},
+		{
+			name: "array int type",
+			args: args{t: "[3]<INT>"},
+			want: []string{"[3]int", "[3]int8", "[3]int16", "[3]int32", "[3]int64", "[3]uint", "[3]uint8", "[3]uint16", "[3]uint32", "[3]uint64"},
+		},
+		{
+			name: "array float type",
+			args: args{t: "[3]<FLOAT>"},
+			want: []string{"[3]float32", "[3]float64"},
+		},
+		{
+			name: "invalid type",
+			args: args{t: "<INVALID>"},
+			want: []string{"invalid"},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := FromNormalizedToBasicTypes(tt.args.t); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("FromNormalizedToBasicTypes() = %v, want %v", got, tt.want)
 			}
 		})
 	}
