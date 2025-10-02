00fe1f8225cd05ee621072b45a62e248da9f0ffb
Alex S. Garzão <alexgarzao@gmail.com>
2025-10-02 18:48:07 -0300

    test: helper funcs to support tests with normalized tests

---
+++ internal/common/helpers.go
@@ -0,0 +1,178 @@
+package common
+
+import (
+	"strings"
+
+	"github.com/opencodeco/validgen/types"
+)
+
+func HelperFromNormalizedToBasicTypes(t string) []string {
+	switch t {
+	case "<STRING>":
+		return []string{"string"}
+	case "<BOOL>":
+		return []string{"bool"}
+	case "<INT>":
+		return []string{"int", "int8", "int16", "int32", "int64", "uint", "uint8", "uint16", "uint32", "uint64"}
+	case "<FLOAT>":
+		return []string{"float32", "float64"}
+	case "map[<STRING>]":
+		return []string{"map[string]"}
+	case "map[<BOOL>]":
+		return []string{"map[bool]"}
+	case "map[<INT>]":
+		return []string{"map[int]", "map[int8]", "map[int16]", "map[int32]", "map[int64]", "map[uint]", "map[uint8]", "map[uint16]", "map[uint32]", "map[uint64]"}
+	case "map[<FLOAT>]":
+		return []string{"map[float32]", "map[float64]"}
+	case "[]<STRING>":
+		return []string{"[]string"}
+	case "[]<BOOL>":
+		return []string{"[]bool"}
+	case "[]<INT>":
+		return []string{"[]int", "[]int8", "[]int16", "[]int32", "[]int64", "[]uint", "[]uint8", "[]uint16", "[]uint32", "[]uint64"}
+	case "[]<FLOAT>":
+		return []string{"[]float32", "[]float64"}
+	}
+
+	// Try to remove [N]
+	if len(t) > 0 && t[0] == '[' {
+		closeBracketIndex := strings.Index(t, "]")
+		if closeBracketIndex == -1 {
+			return []string{"invalid"}
+		}
+
+		size := t[1:closeBracketIndex]
+		basicType := t[closeBracketIndex+1:]
+		sizeInType := "[" + size + "]"
+		result := []string{}
+		switch basicType {
+		case "<STRING>":
+			result = []string{"[N]string"}
+		case "<BOOL>":
+			result = []string{"[N]bool"}
+		case "<INT>":
+			result = []string{"[N]int", "[N]int8", "[N]int16", "[N]int32", "[N]int64", "[N]uint", "[N]uint8", "[N]uint16", "[N]uint32", "[N]uint64"}
+		case "<FLOAT>":
+			result = []string{"[N]float32", "[N]float64"}
+		}
+
+		for i := range result {
+			result[i] = strings.ReplaceAll(result[i], "[N]", sizeInType)
+		}
+
+		return result
+	}
+
+	return []string{"invalid"}
+}
+
+func HelperFromNormalizedToFieldTypes(t string) ([]FieldType, error) {
+	switch t {
+	case "<STRING>":
+		return []FieldType{{BaseType: "string"}}, nil
+	case "<BOOL>":
+		return []FieldType{{BaseType: "bool"}}, nil
+	case "<INT>":
+		return []FieldType{
+			{BaseType: "int"},
+			{BaseType: "int8"},
+			{BaseType: "int16"},
+			{BaseType: "int32"},
+			{BaseType: "int64"},
+			{BaseType: "uint"},
+			{BaseType: "uint8"},
+			{BaseType: "uint16"},
+			{BaseType: "uint32"},
+			{BaseType: "uint64"},
+		}, nil
+	case "<FLOAT>":
+		return []FieldType{
+			{BaseType: "float32"},
+			{BaseType: "float64"},
+		}, nil
+	case "map[<STRING>]":
+		return []FieldType{{BaseType: "string", ComposedType: "map"}}, nil
+	case "map[<BOOL>]":
+		return []FieldType{{BaseType: "bool", ComposedType: "map"}}, nil
+	case "map[<INT>]":
+		return []FieldType{
+			{BaseType: "int", ComposedType: "map"},
+			{BaseType: "int8", ComposedType: "map"},
+			{BaseType: "int16", ComposedType: "map"},
+			{BaseType: "int32", ComposedType: "map"},
+			{BaseType: "int64", ComposedType: "map"},
+			{BaseType: "uint", ComposedType: "map"},
+			{BaseType: "uint8", ComposedType: "map"},
+			{BaseType: "uint16", ComposedType: "map"},
+			{BaseType: "uint32", ComposedType: "map"},
+			{BaseType: "uint64", ComposedType: "map"},
+		}, nil
+	case "map[<FLOAT>]":
+		return []FieldType{
+			{BaseType: "float32", ComposedType: "map"},
+			{BaseType: "float64", ComposedType: "map"},
+		}, nil
+	case "[]<STRING>":
+		return []FieldType{{BaseType: "string", ComposedType: "[]"}}, nil
+	case "[]<BOOL>":
+		return []FieldType{{BaseType: "bool", ComposedType: "[]"}}, nil
+	case "[]<INT>":
+		return []FieldType{
+			{BaseType: "int", ComposedType: "[]"},
+			{BaseType: "int8", ComposedType: "[]"},
+			{BaseType: "int16", ComposedType: "[]"},
+			{BaseType: "int32", ComposedType: "[]"},
+			{BaseType: "int64", ComposedType: "[]"},
+			{BaseType: "uint", ComposedType: "[]"},
+			{BaseType: "uint8", ComposedType: "[]"},
+			{BaseType: "uint16", ComposedType: "[]"},
+			{BaseType: "uint32", ComposedType: "[]"},
+			{BaseType: "uint64", ComposedType: "[]"},
+		}, nil
+	case "[]<FLOAT>":
+		return []FieldType{
+			{BaseType: "float32", ComposedType: "[]"},
+			{BaseType: "float64", ComposedType: "[]"},
+		}, nil
+	}
+
+	// Try to remove [N] (array size)
+	if len(t) > 0 && t[0] == '[' {
+		closeBracketIndex := strings.Index(t, "]")
+		if closeBracketIndex == -1 {
+			return nil, types.NewValidationError("invalid array size: %s", t)
+		}
+
+		size := t[1:closeBracketIndex]
+		basicType := t[closeBracketIndex+1:]
+		result := []FieldType{}
+		switch basicType {
+		case "<STRING>":
+			result = []FieldType{{BaseType: "string", ComposedType: "[N]", Size: size}}
+		case "<BOOL>":
+			result = []FieldType{{BaseType: "bool", ComposedType: "[N]", Size: size}}
+		case "<INT>":
+			result = []FieldType{
+				{BaseType: "int", ComposedType: "[N]", Size: size},
+				{BaseType: "int8", ComposedType: "[N]", Size: size},
+				{BaseType: "int16", ComposedType: "[N]", Size: size},
+				{BaseType: "int32", ComposedType: "[N]", Size: size},
+				{BaseType: "int64", ComposedType: "[N]", Size: size},
+				{BaseType: "uint", ComposedType: "[N]", Size: size},
+				{BaseType: "uint8", ComposedType: "[N]", Size: size},
+				{BaseType: "uint16", ComposedType: "[N]", Size: size},
+				{BaseType: "uint32", ComposedType: "[N]", Size: size},
+				{BaseType: "uint64", ComposedType: "[N]", Size: size},
+			}
+		case "<FLOAT>":
+			result = []FieldType{
+				{BaseType: "float32", ComposedType: "[N]", Size: size},
+				{BaseType: "float64", ComposedType: "[N]", Size: size},
+			}
+		}
+
+		return result, nil
+	}
+
+	return nil, types.NewValidationError("invalid type: %s", t)
+}
---
+++ internal/common/helpers_test.go
@@ -0,0 +1,283 @@
+package common
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestFromNormalizedToBasicTypes(t *testing.T) {
+	type args struct {
+		t string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		{
+			name: "string type",
+			args: args{t: "<STRING>"},
+			want: []string{"string"},
+		},
+		{
+			name: "bool type",
+			args: args{t: "<BOOL>"},
+			want: []string{"bool"},
+		},
+		{
+			name: "int type",
+			args: args{t: "<INT>"},
+			want: []string{"int", "int8", "int16", "int32", "int64", "uint", "uint8", "uint16", "uint32", "uint64"},
+		},
+		{
+			name: "float type",
+			args: args{t: "<FLOAT>"},
+			want: []string{"float32", "float64"},
+		},
+		{
+			name: "map string type",
+			args: args{t: "map[<STRING>]"},
+			want: []string{"map[string]"},
+		},
+		{
+			name: "map bool type",
+			args: args{t: "map[<BOOL>]"},
+			want: []string{"map[bool]"},
+		},
+		{
+			name: "map int type",
+			args: args{t: "map[<INT>]"},
+			want: []string{"map[int]", "map[int8]", "map[int16]", "map[int32]", "map[int64]", "map[uint]", "map[uint8]", "map[uint16]", "map[uint32]", "map[uint64]"},
+		},
+		{
+			name: "map float type",
+			args: args{t: "map[<FLOAT>]"},
+			want: []string{"map[float32]", "map[float64]"},
+		},
+		{
+			name: "slice string type",
+			args: args{t: "[]<STRING>"},
+			want: []string{"[]string"},
+		},
+		{
+			name: "slice bool type",
+			args: args{t: "[]<BOOL>"},
+			want: []string{"[]bool"},
+		},
+		{
+			name: "slice int type",
+			args: args{t: "[]<INT>"},
+			want: []string{"[]int", "[]int8", "[]int16", "[]int32", "[]int64", "[]uint", "[]uint8", "[]uint16", "[]uint32", "[]uint64"},
+		},
+		{
+			name: "slice float type",
+			args: args{t: "[]<FLOAT>"},
+			want: []string{"[]float32", "[]float64"},
+		},
+		{
+			name: "array string type",
+			args: args{t: "[3]<STRING>"},
+			want: []string{"[3]string"},
+		},
+		{
+			name: "array bool type",
+			args: args{t: "[3]<BOOL>"},
+			want: []string{"[3]bool"},
+		},
+		{
+			name: "array int type",
+			args: args{t: "[3]<INT>"},
+			want: []string{"[3]int", "[3]int8", "[3]int16", "[3]int32", "[3]int64", "[3]uint", "[3]uint8", "[3]uint16", "[3]uint32", "[3]uint64"},
+		},
+		{
+			name: "array float type",
+			args: args{t: "[3]<FLOAT>"},
+			want: []string{"[3]float32", "[3]float64"},
+		},
+		{
+			name: "invalid type",
+			args: args{t: "<INVALID>"},
+			want: []string{"invalid"},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := HelperFromNormalizedToBasicTypes(tt.args.t); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("FromNormalizedToBasicTypes() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestFromNormalizedToFieldTypes(t *testing.T) {
+	tests := []struct {
+		name           string
+		normalizedType string
+		want           []FieldType
+	}{
+		{
+			"string type",
+			"<STRING>",
+			[]FieldType{
+				{BaseType: "string", ComposedType: "", Size: ""},
+			},
+		},
+		{
+			"bool type",
+			"<BOOL>",
+			[]FieldType{
+				{BaseType: "bool", ComposedType: "", Size: ""},
+			},
+		},
+		{
+			"int type",
+			"<INT>",
+			[]FieldType{
+				{BaseType: "int", ComposedType: "", Size: ""},
+				{BaseType: "int8", ComposedType: "", Size: ""},
+				{BaseType: "int16", ComposedType: "", Size: ""},
+				{BaseType: "int32", ComposedType: "", Size: ""},
+				{BaseType: "int64", ComposedType: "", Size: ""},
+				{BaseType: "uint", ComposedType: "", Size: ""},
+				{BaseType: "uint8", ComposedType: "", Size: ""},
+				{BaseType: "uint16", ComposedType: "", Size: ""},
+				{BaseType: "uint32", ComposedType: "", Size: ""},
+				{BaseType: "uint64", ComposedType: "", Size: ""},
+			},
+		},
+		{
+			"float type",
+			"<FLOAT>",
+			[]FieldType{
+				{BaseType: "float32", ComposedType: "", Size: ""},
+				{BaseType: "float64", ComposedType: "", Size: ""},
+			},
+		},
+		{
+			"map string type",
+			"map[<STRING>]",
+			[]FieldType{
+				{BaseType: "string", ComposedType: "map", Size: ""},
+			},
+		},
+		{
+			"map bool type",
+			"map[<BOOL>]",
+			[]FieldType{
+				{BaseType: "bool", ComposedType: "map", Size: ""},
+			},
+		},
+		{
+			"map int type",
+			"map[<INT>]",
+			[]FieldType{
+				{BaseType: "int", ComposedType: "map", Size: ""},
+				{BaseType: "int8", ComposedType: "map", Size: ""},
+				{BaseType: "int16", ComposedType: "map", Size: ""},
+				{BaseType: "int32", ComposedType: "map", Size: ""},
+				{BaseType: "int64", ComposedType: "map", Size: ""},
+				{BaseType: "uint", ComposedType: "map", Size: ""},
+				{BaseType: "uint8", ComposedType: "map", Size: ""},
+				{BaseType: "uint16", ComposedType: "map", Size: ""},
+				{BaseType: "uint32", ComposedType: "map", Size: ""},
+				{BaseType: "uint64", ComposedType: "map", Size: ""},
+			},
+		},
+		{
+			"map float type",
+			"map[<FLOAT>]",
+			[]FieldType{
+				{BaseType: "float32", ComposedType: "map", Size: ""},
+				{BaseType: "float64", ComposedType: "map", Size: ""},
+			},
+		},
+		{
+			"slice string type",
+			"[]<STRING>",
+			[]FieldType{
+				{BaseType: "string", ComposedType: "[]", Size: ""},
+			},
+		},
+		{
+			"slice bool type", "[]<BOOL>",
+			[]FieldType{
+				{BaseType: "bool", ComposedType: "[]", Size: ""},
+			},
+		},
+		{
+			"slice int type",
+			"[]<INT>",
+			[]FieldType{
+				{BaseType: "int", ComposedType: "[]", Size: ""},
+				{BaseType: "int8", ComposedType: "[]", Size: ""},
+				{BaseType: "int16", ComposedType: "[]", Size: ""},
+				{BaseType: "int32", ComposedType: "[]", Size: ""},
+				{BaseType: "int64", ComposedType: "[]", Size: ""},
+				{BaseType: "uint", ComposedType: "[]", Size: ""},
+				{BaseType: "uint8", ComposedType: "[]", Size: ""},
+				{BaseType: "uint16", ComposedType: "[]", Size: ""},
+				{BaseType: "uint32", ComposedType: "[]", Size: ""},
+				{BaseType: "uint64", ComposedType: "[]", Size: ""},
+			},
+		},
+		{
+			"slice float type",
+			"[]<FLOAT>", []FieldType{
+				{BaseType: "float32", ComposedType: "[]", Size: ""},
+				{BaseType: "float64", ComposedType: "[]", Size: ""},
+			},
+		},
+		{
+			"array string type",
+			"[3]<STRING>",
+			[]FieldType{
+				{BaseType: "string", ComposedType: "[N]", Size: "3"},
+			},
+		},
+		{
+			"array bool type",
+			"[3]<BOOL>",
+			[]FieldType{
+				{BaseType: "bool", ComposedType: "[N]", Size: "3"},
+			},
+		},
+		{
+			"array int type",
+			"[3]<INT>",
+			[]FieldType{
+				{BaseType: "int", ComposedType: "[N]", Size: "3"},
+				{BaseType: "int8", ComposedType: "[N]", Size: "3"},
+				{BaseType: "int16", ComposedType: "[N]", Size: "3"},
+				{BaseType: "int32", ComposedType: "[N]", Size: "3"},
+				{BaseType: "int64", ComposedType: "[N]", Size: "3"},
+				{BaseType: "uint", ComposedType: "[N]", Size: "3"},
+				{BaseType: "uint8", ComposedType: "[N]", Size: "3"},
+				{BaseType: "uint16", ComposedType: "[N]", Size: "3"},
+				{BaseType: "uint32", ComposedType: "[N]", Size: "3"},
+				{BaseType: "uint64", ComposedType: "[N]", Size: "3"},
+			},
+		},
+		{
+			"array float type",
+			"[3]<FLOAT>",
+			[]FieldType{
+				{BaseType: "float32", ComposedType: "[N]", Size: "3"},
+				{BaseType: "float64", ComposedType: "[N]", Size: "3"},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := HelperFromNormalizedToFieldTypes(tt.normalizedType)
+			if err != nil {
+				t.Errorf("FromNormalizedToFieldTypes() error = %v, wantErr %v", err, nil)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("FromNormalizedToFieldTypes() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
