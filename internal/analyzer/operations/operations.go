3bf3e7d73a2932f58e9ae8e49d0ee7bf446a6149
Alex S. Garzão <alexgarzao@gmail.com>
2025-10-11 12:03:26 -0300

    refact: operations list moved to operations package

---
+++ internal/analyzer/operations/operations.go
@@ -0,0 +1,56 @@
+package operations
+
+import (
+	"slices"
+
+	"github.com/opencodeco/validgen/internal/common"
+)
+
+type Operation struct {
+	CountValues      common.CountValues
+	IsFieldOperation bool
+	ValidTypes       []string
+}
+
+type Operations struct {
+	operations map[string]Operation
+}
+
+func New() *Operations {
+	return &Operations{
+		operations: operationsList,
+	}
+}
+
+func (o *Operations) IsValid(op string) bool {
+	_, ok := o.operations[op]
+
+	return ok
+}
+
+func (o *Operations) IsValidType(op, fieldType string) bool {
+	operation, ok := o.operations[op]
+	if !ok {
+		return false
+	}
+
+	return slices.Contains(operation.ValidTypes, fieldType)
+}
+
+func (o *Operations) IsFieldOperation(op string) bool {
+	operation, ok := o.operations[op]
+	if !ok {
+		return false
+	}
+
+	return operation.IsFieldOperation
+}
+
+func (o *Operations) ArgsCount(op string) common.CountValues {
+	operation, ok := o.operations[op]
+	if !ok {
+		return common.UNDEFINED
+	}
+
+	return operation.CountValues
+}
--- internal/analyzer/operations/operations_list.go
+++ internal/analyzer/operations/operations_list.go
@@ -1,159 +1,111 @@
-package analyzer
+package operations
 
-import "slices"
+import "github.com/opencodeco/validgen/internal/common"
 
-type CountValues int
-
-const (
-	UNDEFINED CountValues = iota
-	ZERO_VALUE
-	ONE_VALUE
-	MANY_VALUES
-)
-
-type Operation struct {
-	CountValues      CountValues
-	IsFieldOperation bool
-	ValidTypes       []string
-}
-
-var operations = map[string]Operation{
+var operationsList = map[string]Operation{
 	"eq": {
-		CountValues:      ONE_VALUE,
+		CountValues:      common.ONE_VALUE,
 		IsFieldOperation: false,
 		ValidTypes:       []string{"<STRING>", "<INT>", "<FLOAT>", "<BOOL>"},
 	},
 	"required": {
-		CountValues:      ZERO_VALUE,
+		CountValues:      common.ZERO_VALUE,
 		IsFieldOperation: false,
 		ValidTypes:       []string{"<STRING>", "<INT>", "<FLOAT>", "[]<STRING>", "[]<INT>", "map[<STRING>]", "map[<INT>]"},
 	},
 	"gt": {
-		CountValues:      ONE_VALUE,
+		CountValues:      common.ONE_VALUE,
 		IsFieldOperation: false,
 		ValidTypes:       []string{"<INT>", "<FLOAT>"},
 	},
 	"gte": {
-		CountValues:      ONE_VALUE,
+		CountValues:      common.ONE_VALUE,
 		IsFieldOperation: false,
 		ValidTypes:       []string{"<INT>", "<FLOAT>"},
 	},
 	"lte": {
-		CountValues:      ONE_VALUE,
+		CountValues:      common.ONE_VALUE,
 		IsFieldOperation: false,
 		ValidTypes:       []string{"<INT>", "<FLOAT>"},
 	},
 	"lt": {
-		CountValues:      ONE_VALUE,
+		CountValues:      common.ONE_VALUE,
 		IsFieldOperation: false,
 		ValidTypes:       []string{"<INT>", "<FLOAT>"},
 	},
 	"min": {
-		CountValues:      ONE_VALUE,
+		CountValues:      common.ONE_VALUE,
 		IsFieldOperation: false,
 		ValidTypes:       []string{"<STRING>", "[]<STRING>", "[]<INT>", "map[<STRING>]", "map[<INT>]"},
 	},
 	"max": {
-		CountValues:      ONE_VALUE,
+		CountValues:      common.ONE_VALUE,
 		IsFieldOperation: false,
 		ValidTypes:       []string{"<STRING>", "[]<STRING>", "[]<INT>", "map[<STRING>]", "map[<INT>]"},
 	},
 	"eq_ignore_case": {
-		CountValues:      ONE_VALUE,
+		CountValues:      common.ONE_VALUE,
 		IsFieldOperation: false,
 		ValidTypes:       []string{"<STRING>"},
 	},
 	"len": {
-		CountValues:      ONE_VALUE,
+		CountValues:      common.ONE_VALUE,
 		IsFieldOperation: false,
 		ValidTypes:       []string{"<STRING>", "[]<STRING>", "[]<INT>", "map[<STRING>]", "map[<INT>]"},
 	},
 	"neq": {
-		CountValues:      ONE_VALUE,
+		CountValues:      common.ONE_VALUE,
 		IsFieldOperation: false,
 		ValidTypes:       []string{"<STRING>", "<BOOL>", "<INT>", "<FLOAT>"},
 	},
 	"neq_ignore_case": {
-		CountValues:      ONE_VALUE,
+		CountValues:      common.ONE_VALUE,
 		IsFieldOperation: false,
 		ValidTypes:       []string{"<STRING>"},
 	},
 	"in": {
-		CountValues:      MANY_VALUES,
+		CountValues:      common.MANY_VALUES,
 		IsFieldOperation: false,
 		ValidTypes:       []string{"<STRING>", "<INT>", "<FLOAT>", "[]<STRING>", "[]<INT>", "[N]<STRING>", "[N]<INT>", "map[<STRING>]", "map[<INT>]"},
 	},
 	"nin": {
-		CountValues:      MANY_VALUES,
+		CountValues:      common.MANY_VALUES,
 		IsFieldOperation: false,
 		ValidTypes:       []string{"<STRING>", "<INT>", "<FLOAT>", "[]<STRING>", "[]<INT>", "[N]<STRING>", "[N]<INT>", "map[<STRING>]", "map[<INT>]"},
 	},
 	"email": {
-		CountValues:      ZERO_VALUE,
+		CountValues:      common.ZERO_VALUE,
 		IsFieldOperation: false,
 		ValidTypes:       []string{"<STRING>"},
 	},
 	"eqfield": {
-		CountValues:      ONE_VALUE,
+		CountValues:      common.ONE_VALUE,
 		IsFieldOperation: true,
 		ValidTypes:       []string{"<STRING>", "<INT>", "<BOOL>"},
 	},
 	"neqfield": {
-		CountValues:      ONE_VALUE,
+		CountValues:      common.ONE_VALUE,
 		IsFieldOperation: true,
 		ValidTypes:       []string{"<STRING>", "<INT>", "<BOOL>"},
 	},
 	"gtefield": {
-		CountValues:      ONE_VALUE,
+		CountValues:      common.ONE_VALUE,
 		IsFieldOperation: true,
 		ValidTypes:       []string{"<INT>"},
 	},
 	"gtfield": {
-		CountValues:      ONE_VALUE,
+		CountValues:      common.ONE_VALUE,
 		IsFieldOperation: true,
 		ValidTypes:       []string{"<INT>"},
 	},
 	"ltefield": {
-		CountValues:      ONE_VALUE,
+		CountValues:      common.ONE_VALUE,
 		IsFieldOperation: true,
 		ValidTypes:       []string{"<INT>"},
 	},
 	"ltfield": {
-		CountValues:      ONE_VALUE,
+		CountValues:      common.ONE_VALUE,
 		IsFieldOperation: true,
 		ValidTypes:       []string{"<INT>"},
 	},
-}
-
-func IsValidOperation(op string) bool {
-	_, ok := operations[op]
-
-	return ok
-}
-
-func IsValidTypeOperation(op, fieldType string) bool {
-	operation, ok := operations[op]
-	if !ok {
-		return false
-	}
-
-	return slices.Contains(operation.ValidTypes, fieldType)
-}
-
-func IsFieldOperation(op string) bool {
-	operation, ok := operations[op]
-	if !ok {
-		return false
-	}
-
-	return operation.IsFieldOperation
-}
-
-func CountValuesByOperation(op string) CountValues {
-	operation, ok := operations[op]
-	if !ok {
-		return UNDEFINED
-	}
-
-	return operation.CountValues
 }
