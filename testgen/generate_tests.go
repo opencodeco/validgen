f6527529138120d8310a54da5ff8485c0bebf0c0
Alex Sandro Garzão <alexgarzao@gmail.com>
2025-11-04 19:30:22 -0300

    chore: add missed \n in error messages

--- testgen/generate_tests.go
+++ testgen/generate_tests.go
@@ -9,22 +9,22 @@ func main() {
 	fmt.Println("Generating tests files")
 
 	if err := generateValidationTypesEndToEndTests(); err != nil {
-		fmt.Printf("error generating validation types end-to-end tests: %s", err)
+		fmt.Printf("error generating validation types end-to-end tests: %s\n", err)
 		os.Exit(1)
 	}
 
 	if err := generateValidationCodeUnitTests(); err != nil {
-		fmt.Printf("error generating validation code unit tests: %s", err)
+		fmt.Printf("error generating validation code unit tests: %s\n", err)
 		os.Exit(1)
 	}
 
 	if err := generateFunctionCodeUnitTests(); err != nil {
-		fmt.Printf("error generating function code unit tests: %s", err)
+		fmt.Printf("error generating function code unit tests: %s\n", err)
 		os.Exit(1)
 	}
 
 	if err := generateComparativePerformanceTests(); err != nil {
-		fmt.Printf("error generating comparative performance tests: %s", err)
+		fmt.Printf("error generating comparative performance tests: %s\n", err)
 		os.Exit(1)
 	}
 
